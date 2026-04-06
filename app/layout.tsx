d557c989b9995e676369ed7fe46d4a4e45cf6da3
Falah Fahrurozi <falahfahrur@gmail.com>
2026-04-06 15:22:19 +0700

    fixed analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export const metadata: Metadata = {
 };
 
 import { AuthProvider } from "@/components/auth/AuthContext";
-import { Analytics } from "@vercel/analytics/react";
+import { Analytics } from "@vercel/analytics/next"
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function RootLayout({
